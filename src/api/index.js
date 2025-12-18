974150b18d55eed142c5f7068f50ddd66a8840c5
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-12-18 23:24:08 +0500

    fix(axios): add env files for prod and dev

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
+import { useAuthStore } from '@/stores/auth';
 
-const api = axios.create();
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+});
 
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem('zhancore_token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use((config) => {
+    const authStore = useAuthStore();
+    if (authStore.token) {
+        config.headers.Authorization = `Bearer ${authStore.token}`;
     }
     return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default api;
